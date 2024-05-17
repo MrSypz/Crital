7902388ce78d8af9257677d5430f7faea6119936
Sypztep <chainot@hotmail.co.th>
2024-05-17 15:11:57 +0700

    Ok I'm back with this system for now and fix a thing don't touch the data again

--- src/main/java/sypztep/crital/common/data/CritOverhaulConfig.java
+++ src/main/java/sypztep/crital/common/data/CritOverhaulConfig.java
@@ -11,47 +11,65 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class CritOverhaulConfig {
-    public static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private static final String CONFIG_FILE_PATH = FabricLoader.getInstance().getConfigDir().resolve("newcritoverhaul/crit_chances.json").toString();
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final String CONFIG_FILE_PATH = FabricLoader.getInstance().getConfigDir().resolve("crital/crit_chances.json").toString();
     private final CritOverhaulData critOverhaulData;
-    private static final CritOverhaulData CLIENT_DEFAULT_DATA = new CritOverhaulData();
+
     public CritOverhaulConfig() {
         this.critOverhaulData = loadConfig();
     }
-    public CritOverhaulData loadConfig() {
+
+
+    private CritOverhaulData loadConfig() {
         try {
             Path configFilePath = Paths.get(CONFIG_FILE_PATH);
+
             if (Files.exists(configFilePath)) {
                 try (Reader reader = new FileReader(configFilePath.toFile())) {
                     return gson.fromJson(reader, CritOverhaulData.class);
                 }
             } else {
-                saveConfig(CLIENT_DEFAULT_DATA);
-                return CLIENT_DEFAULT_DATA;
+                // If the config file doesn't exist, create a default one
+                CritOverhaulData defaultData = new CritOverhaulData();
+                saveConfig(defaultData);
+                return defaultData;
             }
         } catch (IOException e) {
             e.printStackTrace();
-            return new CritOverhaulData();
+            return new CritOverhaulData(); // Return default data in case of an error
         }
     }
 
-    private void saveConfig(CritOverhaulData newData) {
+    private void saveConfig(CritOverhaulData data) {
         try {
             Path configFilePath = Paths.get(CONFIG_FILE_PATH);
             Files.createDirectories(configFilePath.getParent());
 
-            if (!Files.exists(configFilePath) || Files.size(configFilePath) == 0) {
-                Files.createFile(configFilePath);
-                try (Writer writer = new FileWriter(configFilePath.toFile())) {
-                    gson.toJson(newData, writer);
-                }
+            try (Writer writer = new FileWriter(configFilePath.toFile())) {
+                gson.toJson(data, writer);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public CritOverhaulData getCritChanceData() {
+        return critOverhaulData;
+    }
     public CritOverhaulEntry getCritDataForItem(String itemName) {
         return critOverhaulData.getItems().getOrDefault(itemName, new CritOverhaulEntry(0.0f, 0.0f));
+    }
+
+    /**
+     * Adds or updates crit chance and crit damage values for a specific item.
+     *
+     * @param IDItemName The identifier of the item (e.g., "minecraft:iron_sword").
+     * @param critChance The crit chance value to be set for the item.
+     * @param critDamage The crit damage value to be set for the item.
+     */
+    public void addItem(String IDItemName, float critChance, float critDamage) {
+        critOverhaulData.getItems().put(IDItemName, new CritOverhaulEntry(critChance, critDamage));
+        saveConfig(critOverhaulData);
     }
     /**
      * Adds or updates crit chance and crit damage values for a list of items.
