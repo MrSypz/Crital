5f732a4b36ace614f1f18365cf47578d9bc3ccd8
Sypztep <chainot@hotmail.co.th>
2024-05-21 23:52:51 +0700

    Add Data tooltip

--- src/main/java/sypztep/crital/client/TooltipItem.java
+++ src/main/java/sypztep/crital/client/TooltipItem.java
@@ -2,6 +2,7 @@ package sypztep.crital.client;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -11,6 +12,7 @@ import net.minecraft.util.Formatting;
 import org.apache.commons.lang3.mutable.MutableFloat;
 import sypztep.crital.common.CritalMod;
 import sypztep.crital.common.data.CritData;
+import sypztep.crital.common.init.ModConfig;
 import sypztep.crital.common.util.CritalDataUtil;
 
 import java.util.List;
@@ -28,14 +30,18 @@ public class TooltipItem {
         String tier = value.getString(CritData.TIER_FLAG);
         float critChance = value.getFloat(CritData.CRITCHANCE_FLAG);
         float critDamage = value.getFloat(CritData.CRITDAMAGE_FLAG);
-        addCritTooltip(lines, critChance, "crit_chance");
-        addCritTooltip(lines, critDamage, "crit_damage");
+        String critChanceQuality = String.format("%.2f", value.getFloat(CritData.CRITCHANCE_QUALITY_FLAG));
+        String critDamageQuality =String.format("%.2f",value.getFloat(CritData.CRITDAMAGE_QUALITY_FLAG));
+        addCritTooltip(lines, critChance, "crit_chance", "| Quality", critChanceQuality + "%");
+        addCritTooltip(lines, critDamage, "crit_damage", "| Quality", critDamageQuality + "%");
         addFormattedTooltip(lines, tier, "tier_flag", CritData.getTierFormatting(tier), Formatting.BOLD);
     }
-    private static void addCritTooltip(List<Text> lines, double amount, String key) {
+
+    private static void addCritTooltip(List<Text> lines, float amount, String key, String... extraKeys) {
         Formatting color = amount > 0 ? Formatting.DARK_GREEN : Formatting.RED;
-        addFormattedTooltip(lines, amount, key, color);
+        addFormattedTooltip(lines, amount, key, color, extraKeys);
     }
+
     private static void addFormattedTooltip(List<Text> lines, String tier, String key, Formatting color, String... extraKeys) {
         Text tooltip = (Text.translatable(CritalMod.MODID + ".modifytooltip." + key).formatted(Formatting.GRAY).append(Text.literal(" " + tier).formatted(color)));
 
@@ -51,15 +57,24 @@ public class TooltipItem {
 
         lines.add(tooltip);
     }
-    private static void addFormattedTooltip(List<Text> lines, double amount, String key, Formatting color, String... extraKeys) {
+    private static void addFormattedTooltip(List<Text> lines, float amount, String key, Formatting color, String... extraKeys) {
         String formattedAmount = String.format("%.2f", amount);
         MutableFloat mutableFloat = new MutableFloat(formattedAmount);
 
         Text tooltip = Text.literal(" " + mutableFloat + "% ").formatted(color)
                 .append(Text.translatable(CritalMod.MODID + ".modifytooltip." + key).formatted(color));
 
-        for (String extraKey : extraKeys)
-            tooltip = tooltip.copy().append(Text.translatable(CritalMod.MODID + ".modifytooltip." + extraKey).formatted(Formatting.DARK_GREEN));
+        if (extraKeys.length > 0 && ModConfig.CONFIG.itemInfo) {
+            if (Screen.hasShiftDown()) {
+                StringBuilder extraText = new StringBuilder();
+                for (String extraKey : extraKeys) {
+                    extraText.append(" ").append(extraKey);
+                }
+                tooltip = tooltip.copy().append(Text.literal(extraText.toString()).formatted(color));
+            } else {
+                tooltip = tooltip.copy().append(Text.literal(" (Hold Shift)").formatted(Formatting.GRAY));
+            }
+        }
 
         lines.add(tooltip);
     }
