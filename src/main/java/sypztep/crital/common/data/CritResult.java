5238c7408016c5d23023f5c1e22c4c6567560813
Sypztep <chainot@hotmail.co.th>
2024-05-21 23:52:14 +0700

    Add more args

--- src/main/java/sypztep/crital/common/data/CritResult.java
+++ src/main/java/sypztep/crital/common/data/CritResult.java
@@ -1,4 +1,4 @@
 package sypztep.crital.common.data;
 
-public record CritResult(float critChance,float critDamage, CritTier tier) {
+public record CritResult(float critChance, float critDamage, CritTier tier, float critChanceQuality, float critDamageQuality) {
 }
