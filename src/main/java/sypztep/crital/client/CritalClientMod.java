69da978b1feace2664a5a13b401ea5420eebf5db
Sypztep <chainot@hotmail.co.th>
2024-05-22 14:34:25 +0700

    Remove UnUsed rn

--- src/main/java/sypztep/crital/client/CritalClientMod.java
+++ src/main/java/sypztep/crital/client/CritalClientMod.java
@@ -4,15 +4,11 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
-import sypztep.crital.client.gui.GrinderScreen;
 import sypztep.crital.client.gui.StatsScreen;
 import sypztep.crital.client.packets2c.CritSyncPayload;
-import sypztep.crital.common.CritalMod;
-import sypztep.crital.common.data.CritTier;
 
 public class CritalClientMod implements ClientModInitializer {
     public static KeyBinding stats_screen = new KeyBinding("key.crital.stats", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_I, "category.crital.keybind");
@@ -20,12 +16,11 @@ public class CritalClientMod implements ClientModInitializer {
     public void onInitializeClient() {
         ClientPlayNetworking.registerGlobalReceiver(CritSyncPayload.ID, (payload, context) -> CritSyncPayload.receiver(payload,context::client));
         ItemTooltipCallback.EVENT.register((stack, context, type, lines) -> TooltipItem.onTooltipRender(stack,lines,context));
-        HandledScreens.register(CritalMod.GRINDER_SCREEN_HANDLER_TYPE, GrinderScreen::new);
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (stats_screen.wasPressed()) {
                 client.setScreen(new StatsScreen());
             }
         });
-        CritTier.initializeTemplates();
+//        CritTier.initializeTemplates();
     }
 }
--- src/main/java/sypztep/crital/common/CritalMod.java
+++ src/main/java/sypztep/crital/common/CritalMod.java
@@ -2,22 +2,15 @@ package sypztep.crital.common;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.resource.featuretoggle.FeatureFlags;
-import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
 import sypztep.crital.client.packets2c.CritSyncPayload;
 import sypztep.crital.common.init.ModConfig;
-import sypztep.crital.common.screen.GrinderScreenHandler;
 
 import java.util.logging.Logger;
 
 public class CritalMod implements ModInitializer {
     public static final String MODID = "crital";
     public static final Logger LOGGER = Logger.getLogger(MODID);
-    public static final ScreenHandlerType<GrinderScreenHandler> GRINDER_SCREEN_HANDLER_TYPE =
-            new ScreenHandlerType<>(GrinderScreenHandler::new, FeatureFlags.VANILLA_FEATURES);
 
     public static Identifier id (String path) {
         return new Identifier(MODID,path);
@@ -26,7 +19,5 @@ public class CritalMod implements ModInitializer {
     public void onInitialize() {
         PayloadTypeRegistry.playS2C().register(CritSyncPayload.ID, CritSyncPayload.CODEC); // Server to Client
         ModConfig.init();
-
-        Registry.register(Registries.SCREEN_HANDLER, id("grinder"), GRINDER_SCREEN_HANDLER_TYPE);
     }
 }
