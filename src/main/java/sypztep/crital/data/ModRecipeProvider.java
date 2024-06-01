4900e36f94068ae66cc5dcc0c34c355221970c02
chai <chainot@hotmail.co.th>
2024-06-01 20:11:47 +0700

    Datagen

---
+++ src/main/java/sypztep/crital/data/ModRecipeProvider.java
@@ -0,0 +1,46 @@
+package sypztep.crital.data;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
+import sypztep.crital.common.CritalMod;
+import sypztep.crital.common.init.ModItem;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter exporter) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItem.COPPERAL_WEAPON, 1)
+                .pattern("#C")
+                .pattern("C#")
+                .input('#', Items.AMETHYST_SHARD)
+                .input('C', Items.COPPER_BLOCK)
+                .criterion(hasItem(Items.COPPER_INGOT), conditionsFromItem(Items.COPPER_INGOT))
+                .offerTo(exporter, CritalMod.id(getRecipeName(ModItem.COPPERAL_WEAPON)));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItem.COPPERAL_ARMOR, 1)
+                .pattern("#0")
+                .pattern("0#")
+                .input('#', Items.AMETHYST_SHARD)
+                .input('0', Items.OXIDIZED_COPPER)
+                .criterion(hasItem(Items.COPPER_INGOT), conditionsFromItem(Items.COPPER_INGOT))
+                .offerTo(exporter, CritalMod.id(getRecipeName(ModItem.COPPERAL_ARMOR)));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.DECORATIONS, ModItem.GRINDER_TABLE)
+                .pattern("@@")
+                .pattern("##")
+                .pattern("##")
+                .input('#', ItemTags.PLANKS)
+                .input('@', Items.COPPER_INGOT)
+                .criterion(hasItem(Items.COPPER_INGOT), conditionsFromItem(Items.COPPER_INGOT))
+                .offerTo(exporter, CritalMod.id(getRecipeName(ModItem.GRINDER_TABLE)));
+    }
+}
