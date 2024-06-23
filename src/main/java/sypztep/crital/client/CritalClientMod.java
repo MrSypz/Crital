b75b9f3c82a560dd7006f4b81bfa8e9bb2fa9800
chai <chainot@hotmail.co.th>
2024-06-23 14:57:11 +0700

    Delete build-in resourse pack

--- src/main/java/sypztep/crital/client/CritalClientMod.java
+++ src/main/java/sypztep/crital/client/CritalClientMod.java
@@ -4,9 +4,6 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -34,7 +31,6 @@ public class CritalClientMod implements ClientModInitializer {
                 client.setScreen(new StatsScreen());
             }
         });
-        FabricLoader.getInstance().getModContainer(CritalMod.MODID).ifPresent(modContainer -> ResourceManagerHelper.registerBuiltinResourcePack(CritalMod.id("alternate_border"), modContainer, ResourcePackActivationType.NORMAL));
         HandledScreens.register(CritalMod.GRINDER_SCREEN_HANDLER_TYPE, GrinderScreen::new);
     }
 }
