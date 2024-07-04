96db91afe94c6e25d81da586784eaf5529d6b432
chai <chainot@hotmail.co.th>
2024-07-04 19:56:49 +0700

    TODO!

--- src/main/java/sypztep/crital/mixin/vanillachange/newcrit/LivingEntityMixin.java
+++ src/main/java/sypztep/crital/mixin/vanillachange/newcrit/LivingEntityMixin.java
@@ -93,7 +93,7 @@ public abstract class LivingEntityMixin extends Entity implements NewCriticalOve
         for (EquipmentSlot slot : EquipmentSlot.values()) {
             if (ModConfig.exceptoffhandslot && slot == EquipmentSlot.OFFHAND) continue;
             ItemStack itemStack = this.getEquippedStack(slot);
-            if (!itemStack.isEmpty()) {
+            if (!itemStack.isEmpty()) { // TODO : Implement ItemStackHelper to replace this redundance call
                 @Nullable NbtComponent data = itemStack.get(DataComponentTypes.CUSTOM_DATA);
                 if (data != null) {
                     nbtList.add(data.copyNbt());
