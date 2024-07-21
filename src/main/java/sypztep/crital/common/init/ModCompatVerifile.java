9579b065609cbbfe5ee4bfa3f92036de78032e05
chai <chainot@hotmail.co.th>
2024-07-22 06:57:08 +0700

    build 0.7.3 Penomior 0.2.0 compatible

--- src/main/java/sypztep/crital/common/init/ModCompatVerifile.java
+++ src/main/java/sypztep/crital/common/init/ModCompatVerifile.java
@@ -10,13 +10,30 @@ public class ModCompatVerifile {
     public static void init() {
         FabricLoader.getInstance().getModContainer(PENOMIORMODID).ifPresent(modContainer -> {
             String installedVersion = modContainer.getMetadata().getVersion().getFriendlyString();
+            String requiredVersion = "0.1.7";
 
-            if (!installedVersion.equals("0.1.7")) {
-                throw new RuntimeException("Penomior version mismatch. Required: " + "0.1.7" + ", Installed: " + installedVersion);
+            if (compareVersions(installedVersion, requiredVersion) < 0) {
+                throw new RuntimeException("Penomior version mismatch. Required: " + requiredVersion + ", Installed: " + installedVersion);
             }
         });
+
         if (!CritalMod.isPenomiorLoaded) {
             CritalMod.LOGGER.info("Penomior is not installed");
         }
+    }
+
+    private static int compareVersions(String installed, String required) {
+        String[] installedParts = installed.split("\\.");
+        String[] requiredParts = required.split("\\.");
+
+        for (int i = 0; i < Math.max(installedParts.length, requiredParts.length); i++) {
+            int installedPart = i < installedParts.length ? Integer.parseInt(installedParts[i]) : 0;
+            int requiredPart = i < requiredParts.length ? Integer.parseInt(requiredParts[i]) : 0;
+
+            if (installedPart != requiredPart) {
+                return Integer.compare(installedPart, requiredPart);
+            }
+        }
+        return 0;
     }
 }
