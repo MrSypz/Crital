1411148532991b6b9bfaa0f95bad943901868028
chai <chainot@hotmail.co.th>
2024-07-24 12:53:36 +0700

    Tooltip Clean up atleast make it compatible with other thing

--- src/main/java/sypztep/crital/mixin/vanillachange/newcrit/item/ItemStackMixin.java
+++ src/main/java/sypztep/crital/mixin/vanillachange/newcrit/item/ItemStackMixin.java
@@ -1,6 +1,8 @@
 package sypztep.crital.mixin.vanillachange.newcrit.item;
 
-import net.minecraft.component.ComponentMapImpl;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.component.ComponentType;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.NbtComponent;
 import net.minecraft.entity.player.PlayerEntity;
@@ -8,14 +10,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.registry.Registries;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -31,6 +31,8 @@ import java.util.function.Consumer;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
+    @Unique
+    private final ItemStack stack = (ItemStack) (Object) this;
     @Shadow
     public abstract Text getName();
 
@@ -41,25 +43,7 @@ public abstract class ItemStackMixin {
     public abstract ItemStack copy();
 
     @Shadow
-    public abstract boolean isDamaged();
-
-    @Shadow
-    public abstract int getMaxDamage();
-
-    @Shadow
-    public abstract int getDamage();
-
-    @Shadow
     public abstract Item getItem();
-
-    @Shadow
-    @Final
-    ComponentMapImpl components;
-
-    @Shadow
-    @Final
-    private static Text DISABLED_TEXT;
-
     @ModifyVariable(
             method = "getTooltip",
             at = @At("STORE"),
@@ -79,29 +63,31 @@ public abstract class ItemStackMixin {
     @Inject(method = "getTooltip",
             at = @At(value = "INVOKE",
                     target = "Lnet/minecraft/item/ItemStack;appendTooltip(Lnet/minecraft/component/ComponentType;Lnet/minecraft/item/Item$TooltipContext;Ljava/util/function/Consumer;Lnet/minecraft/item/tooltip/TooltipType;)V",
-                    shift = At.Shift.BY, by = 4),
-            locals = LocalCapture.CAPTURE_FAILHARD,
-            cancellable = true)
-    private void replaceAppendEnchantmentTooltip(Item.TooltipContext context, @Nullable PlayerEntity player, TooltipType type, CallbackInfoReturnable<List<Text>> cir, List<Text> list, MutableText mutableText, Consumer<Text> consumer) {
+                    ordinal = 3),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    private void replaceAppendEnhancementTooltip(Item.TooltipContext context, @Nullable PlayerEntity player, TooltipType type, CallbackInfoReturnable<List<Text>> cir, List<Text> list, MutableText mutableText, Consumer<Text> consumer) {
         if (!ModConfig.NewToolTip)
             return;
-        ItemStack stack = (ItemStack) (Object) this;
         if (stack.contains(DataComponentTypes.CUSTOM_DATA)) {
             CritalTooltipRender.getTooltip(stack, list, context);
-
-            //I'm too lazy to modify mixin to copy paste from getTooltip it self
-            if (type.isAdvanced()) {
-                if (this.isDamaged())
-                    list.add(Text.translatable("item.durability", this.getMaxDamage() - this.getDamage(), this.getMaxDamage()));
-                list.add(Text.literal(Registries.ITEM.getId(this.getItem()).toString()).formatted(Formatting.DARK_GRAY));
-                int i = this.components.size();
-                if (i > 0)
-                    list.add(Text.translatable("item.components", i).formatted(Formatting.DARK_GRAY));
-            }
-            if (player != null && !this.getItem().isEnabled(player.getWorld().getEnabledFeatures()))
-                list.add(DISABLED_TEXT);
-
-            cir.setReturnValue(list);
         }
+    }
+    @WrapOperation(method = "getTooltip",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/item/ItemStack;appendTooltip(Lnet/minecraft/component/ComponentType;Lnet/minecraft/item/Item$TooltipContext;Ljava/util/function/Consumer;Lnet/minecraft/item/tooltip/TooltipType;)V",
+                    ordinal = 3)
+    )
+    private void removeEnchantmentTooltip(ItemStack instance, ComponentType<?> componentType, Item.TooltipContext context, Consumer<Text> textConsumer, TooltipType type, Operation<Void> original) {
+        if (!ModConfig.NewToolTip) // if config is disable call default
+            original.call(instance, componentType, context, textConsumer, type);
+    }
+    @WrapOperation(method = "getTooltip",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/item/ItemStack;appendAttributeModifiersTooltip(Ljava/util/function/Consumer;Lnet/minecraft/entity/player/PlayerEntity;)V")
+    )
+    private void removeAttributeModifiersTooltip(ItemStack instance, Consumer<Text> textConsumer, PlayerEntity player, Operation<Void> original) {
+        if (!ModConfig.NewToolTip) // if config is disable call default
+            original.call(instance, textConsumer, player);
     }
 }
