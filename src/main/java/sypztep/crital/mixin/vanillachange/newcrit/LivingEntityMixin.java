41c82f52edabfe22ba9de815752d1cd4f26d7239
chai <chainot@hotmail.co.th>
2024-07-25 14:34:49 +0700

    Handle remove old Data

--- src/main/java/sypztep/crital/mixin/vanillachange/newcrit/LivingEntityMixin.java
+++ src/main/java/sypztep/crital/mixin/vanillachange/newcrit/LivingEntityMixin.java
@@ -141,18 +141,30 @@ public abstract class LivingEntityMixin extends Entity implements NewCriticalOve
         }
     }
 
+    @Unique @Deprecated
+    private static final String HEALTH_FLAG = CritalMod.MODID + "Health_Flag";
+
     @Inject(method = "getEquipmentChanges", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;applyAttributeModifiers(Lnet/minecraft/entity/EquipmentSlot;Ljava/util/function/BiConsumer;)V"), locals = LocalCapture.CAPTURE_FAILHARD)
     private void LivingEntityOnEquipmentChange(CallbackInfoReturnable<Map<EquipmentSlot, ItemStack>> cir) {
         if (!ModConfig.uniqueStats) return;
 
-        List<NbtCompound> equippedNbt = CritalDataUtil.getUniquebtFromEquippedSlots((LivingEntity) (Object) this);
+        LivingEntity livingEntity = (LivingEntity) (Object) this;
+        List<NbtCompound> equippedNbt = CritalDataUtil.getUniquebtFromEquippedSlots(livingEntity);
+
+        // Remove the HEALTH_FLAG from each equipped NBT compound
+        for (NbtCompound nbt : equippedNbt) { // Handle for old mod
+            nbt.remove(HEALTH_FLAG);
+        }
 
         for (Map.Entry<String, UniqueStats> entry : CritalUniqueStats.attributes.entrySet()) {
             UniqueStats uniqueStats = entry.getValue();
             for (RegistryEntry<EntityAttribute> attribute : uniqueStats.getAttributes()) {
                 float totalValue = 0;
-                for (NbtCompound nbt : equippedNbt)
-                    if (nbt.contains(entry.getKey())) totalValue += nbt.getFloat(entry.getKey());
+                for (NbtCompound nbt : equippedNbt) {
+                    if (nbt.contains(entry.getKey())) {
+                        totalValue += nbt.getFloat(entry.getKey());
+                    }
+                }
                 float modifiedValue = uniqueStats.modifyTotalValue(totalValue, attribute);
                 EntityAttributeInstance attributeInstance = this.getAttributeInstance(attribute);
                 if (attributeInstance != null) {
@@ -160,9 +172,10 @@ public abstract class LivingEntityMixin extends Entity implements NewCriticalOve
                     CritalDataUtil.ReplaceAttributeModifier(attributeInstance, mod);
                 }
             }
-            uniqueStats.applyLogic((LivingEntity) (Object) this, equippedNbt);
+            uniqueStats.applyLogic(livingEntity, equippedNbt);
         }
     }
+
 
     /*------------------------------Registry Attribute------------------------------------*/
     @Inject(method = "createLivingAttributes", at = @At(value = "RETURN"), cancellable = true)
